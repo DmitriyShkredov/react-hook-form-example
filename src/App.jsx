9c506614ea4b1cc4724846a0a4f902069762cb63
DmitriyShkredov <dmitriyshkredovdev@gmail.com>
2023-11-25 12:04:14 +0300

    feat: add form validation

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,32 @@
-function App() {
-  return <section></section>;
-}
+import React from "react";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
 
-export default App;
+const schema = yup.object({
+  name: yup
+    .string()
+    .required("Введите имя")
+    .min(2, "Мин 2 буквы")
+    .max(10, "Макс 10 букв")
+    .matches(/^[a-zA-Zа-яА-Я]*$/, "Только буквы"),
+});
+
+export const App = () => {
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm({ mode: "onChange", resolver: yupResolver(schema) });
+  const onSubmit = (data) => console.log(data);
+
+  return (
+    <section>
+      <form className="form" onSubmit={handleSubmit(onSubmit)}>
+        <input type="text" {...register("name")} />
+        <p>{errors.name?.message}</p>
+        <button type="submit">Отправить</button>
+      </form>
+    </section>
+  );
+};
